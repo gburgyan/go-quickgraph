1c14302171bf2ea4357da35144056416832be28d
George Burgyan <gburgyan@deem.com>
2023-09-27 09:03:21 -0600

    Add benchmark for missing processors with caching.

--- benchmark_test.go
+++ benchmark_test.go
@@ -101,3 +101,33 @@ mutation CreateReviewForEpisode($ep: Episode!, $review: ReviewInput!) {
 		_, _ = g.ProcessRequest(ctx, input, vars)
 	}
 }
+
+func BenchmarkMissingProcessor(b *testing.B) {
+	ctx := context.Background()
+	g := Graphy{
+		RequestCache: simpleCache{
+			values: map[string]*simpleCacheEntry{},
+		},
+	}
+
+	input := `
+mutation CreateReviewForEpisode($ep: Episode!, $review: ReviewInput!) {
+  createReview(episode: $ep, review: $review) {
+    stars
+    commentary
+  }
+}`
+
+	vars := `
+{
+  "ep": "JEDI",
+  "review": {
+    "stars": 5,
+    "commentary": "This is a great movie!"
+  }
+}`
+
+	for i := 0; i < b.N; i++ {
+		_, _ = g.ProcessRequest(ctx, input, vars)
+	}
+}
